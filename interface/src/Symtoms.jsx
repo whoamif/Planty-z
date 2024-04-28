c10f90bcd65d4ff47ea7459ba982eb1a6ddfb9ce
whoamif <adjissifatimaamina@gmail.com>
2024-04-28 15:28:58 +0100

    up

--- interface/src/Symtoms.jsx
+++ interface/src/Symtoms.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import "./App.css";
-import { Navigation} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -8,50 +6,54 @@ import plant1 from './assets/img1.png';
 import plant2 from './assets/img2.png';
 import plant3 from './assets/img3.png';
 
-
+const symptoms = [
+    { name: "YellowOrangeBrownPustules", image: symptom1 },
+    { name: "Defoliation", image: symptom2 },
+    { name: "CircularIrregularlySpots", image: symptom3 },
+    { name: "GrayBrownFuzz", image: symptom4 },
+    { name: "StuntedFuzzyGrowth", image: symptom5 },
+    { name: "YellowSpots", image: symptom6 },
+    { name: "YellowBrownSpots", image: symptom7 },
+    { name: "MustyOdor", image: symptom8 },
+    { name: "DarkMushyDecayedRoots", image: symptom9 },
+    { name: "CircularSpots", image: symptom10 },
+    { name: "WaterSoakedSpots", image: symptom11 },
+    { name: "DarkColoredSap", image: symptom12 },
+    { name: "BrownBlackSpots", image: symptom13 },
+    { name: "GreyPurpleFuzz", image: symptom14 },
+    { name: "FuzzyWhiteMold", image: symptom15 },
+    { name: "CurlyLeaves", image: symptom16 },
+    { name: "OnlyOneSideIsAffected", image: symptom17 },
+    { name: "AllThePlantIsAffected", image: symptom18 },
+    { name: "BrownStricksOnViscularTissue", image: symptom19 },
+    { name: "PinkishSporeMasses", image: symptom20 },
+    { name: "Wilting", image: symptom21 },
+    { name: "YellowingOfLeaves", image: symptom22 },
+    { name: "StuntedGrowth", image: symptom23 },
+    { name: "RottingOfRoots", image: symptom24 },
+    { name: "WhitePowderySubstanceOnLeaves", image: symptom25 },
+    { name: "YellowingOfLowerLeaves", image: symptom26 }
+    // Ajoutez les autres symptômes avec leurs images correspondantes ici...
+];
 function Symtoms() {
-  return (
-    <Swiper
-      modules={[Navigation]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      className=' w-5/6 flex m-auto '
-    >
-      <SwiperSlide>
-        <div className=' p-20 flex flex-col gap-4  cursor-pointer hover:shadow-2xl'>
-          <img className='' src={plant1} alt="Plant 1" />
-          <p className='text-black font-bold'>Plant Name 1</p>
-          <p className='text-black font-thin'>Description of plant 1...</p>
-         
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className='p-20 flex flex-col gap-4  cursor-pointer hover:shadow-2xl'>
-          <img className='' src={plant2} alt="Plant 2" />
-          <p className='text-black font-bold'>Plant Name 2</p>
-          <p className='text-black font-thin'>Description of plant 2...</p>
-          
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className='p-20 flex flex-col gap-4  cursor-pointer hover:shadow-2xl'>
-          <img className='' src={plant3} alt="Plant 3" />
-          <p className='text-black font-bold'>Plant Name 3</p>
-          <p className='text-black font-thin'>Description of plant 3...</p>
-         
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className='p-20 flex flex-col gap-4  cursor-pointer hover:shadow-2xl'>
-          <img className='' src={plant3} alt="Plant 3" />
-          <p className='text-black font-bold'>Plant Name 3</p>
-          <p className='text-black font-thin'>Description of plant 3...</p>
-         
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  );
+    return (
+        <Swiper 
+            modules={[Navigation]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            className='w-5/6 flex m-auto'
+        >
+            {symptoms.map((symptom, index) => (
+                <SwiperSlide key={index}>
+                    <div className='p-20 flex flex-col gap-4 cursor-pointer'>
+                        <img className='hover:shadow-2xl' src={symptom.image} alt={symptom.name} />
+                        <p className='text-black font-bold'>{symptom.name}</p>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
 }
 
 export default Symtoms;
